eed44df5bab180fc0b9a2bca10bc8dcddb11c46d
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2021-06-11 08:51:52 -0500

    Update VertxUtils.java

--- Vertx-EventBus-3.8/src/main/java/com/nr/instrumentation/vertx/VertxUtils.java
+++ Vertx-EventBus-3.8/src/main/java/com/nr/instrumentation/vertx/VertxUtils.java
@@ -2,6 +2,20 @@ package com.nr.instrumentation.vertx;
 
 public class VertxUtils {
 
+	private static final String REPLY = "vertx.reply";
+	
+	public static String normalize(String address) {
+		if(address.contains(REPLY)) {
+			return REPLY;
+		} else if(tempAddress(address)) {
+			return "TempAddress";
+		}
+		return address;
+	}
+	
+	public static boolean replyAddress(String address) {
+		return address.contains(REPLY);
+	}
 	
 	public static boolean tempAddress(String address) {
 		for(int i=0;i<address.length();i++) {
@@ -13,5 +27,4 @@ public class VertxUtils {
 		return true;
 	}
 
-	
 }
